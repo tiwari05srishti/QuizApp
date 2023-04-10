1297eb82dfd82d872e1934b102adec2b1b14fb56
tiwari05srishti <102760246+tiwari05srishti@users.noreply.github.com>
2023-04-10 18:23:44 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,16 +2,22 @@ package com.example.qyuizzer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     public Button buttonstrt;
     public Button highscore;
+    public TextView textview_hs;
+    public String text_hs ;
+    public int onclick_count =0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         buttonstrt =  findViewById(R.id.startbtn);
         highscore =  findViewById(R.id.highscores);
-
+        textview_hs = findViewById(R.id.textView4);
 
         buttonstrt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,12 +36,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(ques1intent);
             }
         });
-//        highscore.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //
-//            }
-//        });
+
+        SharedPreferences sp = getSharedPreferences("HIGHSCORE" , MODE_PRIVATE);
+        int hs = sp.getInt("KEY_HIGHSCORE", 0);
+        String hs_str = ""+hs ;
+        text_hs = hs_str +"/5";
+
+        highscore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onclick_count++;
+                if(onclick_count%2!=0){
+                    textview_hs.setText(text_hs);
+                }else{
+                    textview_hs.setText("");
+                }
+
+            }
+        });
 
 
 
