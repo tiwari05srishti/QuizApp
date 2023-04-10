196f7e14057b786e3d0a2f296a5c47f1e2c564e6
tiwari05srishti <102760246+tiwari05srishti@users.noreply.github.com>
2023-04-10 18:25:05 +0530

    Update SplashActivity.java

--- SplashActivity.java
+++ SplashActivity.java
@@ -22,6 +22,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(mainpage);
                 finish();
             }
-        },5000);
+        },4000);
     }
 }
